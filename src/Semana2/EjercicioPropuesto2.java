52066fbc42f7ce91f1324460ed20ede1426acecc
jhvindas <ecko-05@hotmail.com>
2025-02-04 03:58:51 +0000

    EjercicioPropuesto2

--- src/Semana2/EjercicioPropuesto2.java
+++ src/Semana2/EjercicioPropuesto2.java
@@ -3,7 +3,8 @@ package Semana2;
 public class EjercicioPropuesto2 {
     public static void main(String[] args) {
         
-        
+        int EdadAna = X + 10;
+        int EdadElena = 
 
 
     }
