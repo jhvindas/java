612c61800a1ac5c16f2c05f48bd2a1ca1ce34058
jhvindas <ecko-05@hotmail.com>
2025-02-11 03:58:48 +0000

    EjercicioPropuesto14Resuelto

---
+++ src/Semana3/EjercicioPropuesto14.java
@@ -0,0 +1,31 @@
+package Semana3;
+
+import generico.Utils;
+
+public class EjercicioPropuesto14 {
+    public static void main(String[] args) {
+        
+        double horaEntrada;
+        double horaSalida;
+        double tarifaHora;
+        double horaExtra;
+        double dineroTotal;
+        double dineroTotalExtras;
+        double horasTrabajadas;
+
+        horaEntrada = Utils.leerDoble("Digite la hora de entrada: ");
+        horaSalida = Utils.leerDoble("Digite la hora de salida: ");
+        tarifaHora = Utils.leerDoble("Digite la tarifa que se paga por hora: ");
+
+        horaExtra = tarifaHora * 1.5;
+        horasTrabajadas = (horaSalida - horaEntrada);
+        dineroTotal = horasTrabajadas * tarifaHora;
+        dineroTotalExtras = horaExtra * horasTrabajadas;
+
+        if (horasTrabajadas > 8) {
+            System.out.println("Felipe trabajó " + horasTrabajadas + " hora/s, y el pago es de $" + dineroTotalExtras);
+        } else {
+            System.out.println("Felipe trabajó " + horasTrabajadas + " hora/s, y el pago es de $" + dineroTotal);
+        }
+    }
+}
